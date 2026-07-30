afc04635f18e0b02cedcb3e18313200232fa0d13
zntrgt <turgut.ozan@gmail.com>
2026-07-30 14:56:31 +0300

    Generate production smoke URLs from blog manifest

---
+++ scripts/production-smoke-urls.js
@@ -0,0 +1,17 @@
+import { indexableBlogPosts, blogRoute, validateBlogManifest } from '../content/blog-manifest.js';
+
+validateBlogManifest();
+
+const preferred = [
+  ...indexableBlogPosts.filter((post) => post.generator === 'core').slice(0, 1),
+  ...indexableBlogPosts.filter((post) => post.generator === 'benefits').slice(0, 1)
+];
+
+const urls = [
+  'https://maasim.net/version.json',
+  'https://maasim.net/blog/',
+  'https://maasim.net/sitemap.xml',
+  ...preferred.map((post) => `https://maasim.net${blogRoute(post)}`)
+];
+
+for (const url of [...new Set(urls)]) console.log(url);
