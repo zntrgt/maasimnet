c14854b3f7ec751fc37d93b670863e75d5b4eb9f
zntrgt <turgut.ozan@gmail.com>
2026-07-31 00:26:45 +0300

    Include salary negotiation article in blog pipeline

--- scripts/render-blog.js
+++ scripts/render-blog.js
@@ -2,6 +2,7 @@ import { addBlog } from './add-blog.js';
 import { addFinancialHealthBlog } from './add-financial-health-blog.js';
 import { addCoreBlogCluster } from './add-core-blog-cluster.js';
 import { addBenefitsBlogCluster } from './add-benefits-blog-cluster.js';
+import { addSalaryNegotiationBlog } from './add-salary-negotiation-blog.js';
 import { ensureBlogIndex } from './ensure-blog-index.js';
 import { ensureBlogBreadcrumbSchema } from './ensure-blog-breadcrumb-schema.js';
 import { applyBlogImages } from './apply-blog-images.js';
@@ -17,6 +18,7 @@ export async function renderBlog(dist) {
   await addFinancialHealthBlog(dist);
   await addCoreBlogCluster(dist);
   await addBenefitsBlogCluster(dist);
+  await addSalaryNegotiationBlog(dist);
 
   await ensureBlogIndex(dist);
   await ensureBlogBreadcrumbSchema(dist);
