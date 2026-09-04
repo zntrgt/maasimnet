c7cded2bf4a171392cab2fe5e8c81ffb382405d6
zntrgt <turgut.ozan@gmail.com>
2026-09-04 11:39:25 +0300

    Ship Cloudflare static asset _headers with production build

--- scripts/build.js
+++ scripts/build.js
@@ -43,6 +43,7 @@ await rm(distDir, { recursive: true, force: true });
 await cp(staticDir, distDir, { recursive: true });
 await cp(join(root, 'content', 'robots.txt'), join(distDir, 'robots.txt'));
 await cp(join(root, 'content', 'BingSiteAuth.xml'), join(distDir, 'BingSiteAuth.xml'));
+await cp(join(root, 'content', '_headers'), join(distDir, '_headers'));
 await mkdir(assetsDir, { recursive: true });
 
 for (const file of [
